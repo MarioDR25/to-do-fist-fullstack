6f0759c7bef875079a9747dd2b1f620a05497296
Mario Daza <mariodaza525@gmail.com>
2026-04-27 03:42:01 +0200

    creacion de Metodos Http

--- frontend/src/data/services/api.ts
+++ frontend/src/data/services/api.ts
@@ -1,6 +1,8 @@
-import { Task } from "@/types/task";
-import { TaskDto } from "../dtos/task.dto";
-import { mapTaskDtoToTask } from "../mappers/mapTaskDtoToTask";
+import { taskDataToTaskDataDto } from "../mappers/taskDataToTaskDataDto";
+import { TaskRequestDto, TaskDto } from "../dtos/taskDto";
+import { taskDtoToTask } from "../mappers/taskDtoToTask";
+import  {TaskRequest, Task}  from "@/types/task";
+
 
 const BASE_URL: string = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
 
@@ -12,7 +14,47 @@ export const getAllTasks = async (): Promise<Task[]> => {
     }
     const data: TaskDto[] = await response.json()
  
-    const tasks = data.map(t => mapTaskDtoToTask(t));
+    const tasks: Task[] = data.map(t => taskDtoToTask(t));
     
     return tasks;
 } 
+
+
+
+export const deleteTask = async (id: number): Promise<void> => {
+    const response = await fetch(`${BASE_URL}/tasks/delete/${id}`, {method: 'DELETE'})
+    if (!response.ok) {
+        throw new Error(`No se pudo ELiminar: ${response.status} ${response.statusText}`)
+    }
+}
+
+
+export const addTask = async (task: TaskRequest): Promise<void> => {
+    const data: TaskRequestDto = taskDataToTaskDataDto(task)
+
+    const response = await fetch(`${BASE_URL}/tasks/add`, 
+        {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data, null, 2)
+        } )
+    if (!response.ok) {
+        throw new Error(`No se pudo Agregar la tarea: ${response.status} ${response.statusText}`)
+    }
+}
+
+
+
+export const updateTask = async (id: number, task: TaskRequest): Promise<void> => {
+    const data: TaskRequestDto = taskDataToTaskDataDto(task)
+
+    const response = await fetch(`${BASE_URL}/tasks/modify/${id}`, 
+        {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data, null, 2)
+        } )
+    if (!response.ok) {
+        throw new Error(`No se pudo Actualizar la tarea: ${response.status} ${response.statusText}`)
+    }
+}
